202b035e88561a84b96b330e1cba20945ee26cd4
Abrilskop <uberpot.ia@gmail.com>
2024-11-27 23:10:10 -0500

    feat: add interfaces

---
+++ extend/menu_admin.php
@@ -0,0 +1,39 @@
+<nav>
+  <a href="" data-activates="menu" class="button-collpase" ><i class="material-icons">menu</i></a>
+</nav>
+<ul id="menu" class="side-nav fixed" >
+  <li>
+    <div class="userView" >
+      <div class="background">
+        <img src="../img/logo.jpeg"  >
+      </div>
+      <a href="" ><img src="../img/gia.jpeg" class="circle" alt=""></a>
+      <br>
+      <br>
+      <br>
+      <a href="" class="black-text" >Gianella Alexandra</a>
+      <a href="" class="black-text">uberpot.ia@gmail.com</a>
+    </div>
+  </li>
+  <!-- implementacion guia -->
+  <li><a href="./login.html"><i class="material-icons">home</i> INICIO</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="../usuarios/index.php"><i class="material-icons">person_add</i> ALTA USUARIOS</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="../productos/index.php"><i class="material-icons">add_box</i> ALTA PRODUCTOS</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="../clientes/index.php"><i class="material-icons">supervised_user_circle</i> ALTA CLIENTES</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="../ventas/index.php"><i class="material-icons">attach_money</i> ALTA VENTAS</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="../detalleventa/index.php"><i class="material-icons">assignment</i> ALTA DETALLEVENTA</a></li>
+  <li><div class="divider"></div></li>
+
+  <li><a href="salir.php"><i class="material-icons">exit_to_app</i> SALIR</a></li>
+  <li><div class="divider"></div></li>
+</ul>
