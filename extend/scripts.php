621ebb282e483d5e28e2b4f90b1670082777e715
Abrilskop <uberpot.ia@gmail.com>
2024-11-27 23:10:28 -0500

    feat: add scripts

---
+++ extend/scripts.php
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script> 
+		<!-- <script type="text/javascript" src="../js/sweetalert2.js"></script> -->
+		<script type="text/javascript" src="../js/materialize.min.js"></script>
+		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.3.2/sweetalert2.js"></script> -->
+		<script type="text/javascript" src="../js/scripts.js"></script>
+		<!-- agregar validacion y verificacion -->
+		<script type="text/javascript" src="../js/validacion.js"></script>
+</main>
+</body>
