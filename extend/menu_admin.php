a643e6e8fedc689ffdeaf6f4dd4a2edc3c1ed9d2
Abrilskop <uberpot.ia@gmail.com>
2024-12-15 23:17:28 -0500

    chore: add swetalert in scripts and change in others

--- extend/menu_admin.php
+++ extend/menu_admin.php
@@ -1,7 +1,7 @@
 <nav>
   <a href="" data-activates="menu" class="button-collpase" ><i class="material-icons">menu</i></a>
 </nav>
-<ul id="menu" class="side-nav fixed" >
+<ul id="menu" class="side-nav fixed grey lighten-4" >
   <li>
     <div class="userView" >
       <div class="background">
@@ -15,25 +15,25 @@
       <a href="" class="black-text">uberpot.ia@gmail.com</a>
     </div>
   </li>
-  <!-- implementacion guia -->
-  <li><a href="./login.html"><i class="material-icons">home</i> INICIO</a></li>
+  <li><a href="../inicio/inicio.php"><i class="material-icons">home</i> INICIO</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="../usuarios/index.php"><i class="material-icons">person_add</i> ALTA USUARIOS</a></li>
+  <li><a href="../clientes/index.php"><i class="material-icons">person_add</i> ALTA DE CLIENTES</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="../productos/index.php"><i class="material-icons">add_box</i> ALTA PRODUCTOS</a></li>
+  <li><a href="../detalleVenta/index.php"><i class="material-icons">insert_chart</i> DETALLE DE VENTA</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="../clientes/index.php"><i class="material-icons">supervised_user_circle</i> ALTA CLIENTES</a></li>
+  <li><a href="../productos/indexp.php"><i class="material-icons">category</i> ALTA DE PRODUCTOS</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="../ventas/index.php"><i class="material-icons">attach_money</i> ALTA VENTAS</a></li>
+  <li><a href="../usuarios/index.php"><i class="material-icons">group_add</i> ALTA USUARIOS</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="../detalleventa/index.php"><i class="material-icons">assignment</i> ALTA DETALLEVENTA</a></li>
+  <li><a href="../venta/indexv.php"><i class="material-icons">shopping_cart</i> ALTA DE VENTA</a></li>
   <li><div class="divider"></div></li>
 
-  <li><a href="salir.php"><i class="material-icons">exit_to_app</i> SALIR</a></li>
+  <li><a href="../index.php"><i class="material-icons">exit_to_app</i> Salir</a></li>
   <li><div class="divider"></div></li>
+
 </ul>
--- extend/scripts.php
+++ extend/scripts.php
@@ -1,9 +1,7 @@
-<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script> 
-		<!-- <script type="text/javascript" src="../js/sweetalert2.js"></script> -->
-		<script type="text/javascript" src="../js/materialize.min.js"></script>
-		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.3.2/sweetalert2.js"></script> -->
-		<script type="text/javascript" src="../js/scripts.js"></script>
-		<!-- agregar validacion y verificacion -->
-		<script type="text/javascript" src="../js/validacion.js"></script>
+
 </main>
-</body>
+	<script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../js/sweetalert2.js"></script>
+	<script type="text/javascript" src="../js/materialize.min.js"></script>
+	<script type="text/javascript" src="../js/scripts.js"></script>
+	<script type="text/javascript" src="../js/validacion.js"></script>
