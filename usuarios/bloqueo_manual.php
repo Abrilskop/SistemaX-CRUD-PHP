d162de236cb6f1775ca9e1b268d4f83d274faa54
Abrilskop <uberpot.ia@gmail.com>
2024-12-15 23:17:00 -0500

    refactor: add funtion of select in usuarios

--- usuarios/bloqueo_manual.php
+++ usuarios/bloqueo_manual.php
@@ -1,22 +1,22 @@
-<?php
-include '../conexion/conexion.php';
+<?php include '../conexion/conexion.php';
 $id = $con->real_escape_string(htmlentities($_GET['us']));
 $bloqueo = $con->real_escape_string(htmlentities($_GET['bl']));
 
 if ($bloqueo == 1) {
-    $up = $con->query("UPDATE usuario SET bloqueo=0 WHERE idUsuario='$id' ");
-    if ($up) {
-        header('location:../extend/alerta.php?msj=El usuario ha sido bloqueado&c=us&p=in&t=success');
-    } else {
-        header('location:../extend/alerta.php?msj=El usuario no ha podido ser bloqueado&c=us&p=in&t=error');
-    }
-} else {
-    $up = $con->query("UPDATE usuario SET bloqueo=1 WHERE idUsuario='$id' ");
-    if ($up) {
-        header('location:../extend/alerta.php?msj=El usuario ha sido desbloqueado&c=us&p=in&t=success');
-    } else {
-        header('location:../extend/alerta.php?msj=El usuario no ha podido ser desbloqueado&c=us&p=in&t=error');
-    }
+	$up = $con->query("UPDATE usuario SET bloqueo=0 WHERE idUsuario='$id' ");
+	if ($up) {
+		header('location:../extend/alerta.php?msj=El Usuario ha sido bloqueado&c=us&p=in&t=success');
+	}else{
+		header('location:../extend/alerta.php?msj=El Usuario no ha sido bloqueado&c=us&p=in&t=error');
+	}
+}
+else{
+	$up = $con->query("UPDATE usuario SET bloqueo=1 WHERE idUsuario='$id' ");
+	if ($up) {
+		header('location:../extend/alerta.php?msj=El Usuario ha sido bloqueado&c=us&p=in&t=success');
+	}else{
+		header('location:../extend/alerta.php?msj=El Usuario no ha sido bloqueado&c=us&p=in&t=error');
+	}
 }
 $con->close();
-?>
+ ?>
--- usuarios/index.php
+++ usuarios/index.php
@@ -1,24 +1,22 @@
-<!--llama a la cabecera-->  
-<?php 
-include '../conexion/conexion.php';
-include "../extend/header.php";
-?>
-<!--pagina principal-->  
+<?php include "../extend/header.php";
+include "../conexion/conexion.php"; ?>
+
+<!--Panel-->
 <div class="row">
   <div class="col s12">
     <div class="card">
       <div class="card-content">
-        <span class="card-title">ALTA DE USUAROS</span>
-        <form class="form" action="ins_usuarios.php" method="post" enctype="multipart/form-data" autocomplete="off">
+        <span class="card-title">ALTA DE USUARIOS</span>
+        <form class="form" action="ins_usuario.php" method="post" enctype="multipart/form-data" autocomplete="off">
           <div class="input-field">
-            <input type="text" name="nick" required autofocus title="DEBE DE CONTENER ENTRE 8 Y 15 CARACTERES, SOLO LETRAS" pattern="[A-Za-z]{8,15}" id="nick" onblur="may(this.value, this.id)" >
+            <input type="text" name="nick" required autofocus title="DEBE DE CONTENER ENTRE 1 Y 15 CARACTERES, SOLO LETRAS" pattern="[A-Za-z]{1,15}" id="nick" onblur="may(this.value, this.id)" >
             <label for="nick">Nick:</label>
           </div>
 
           <div class="validacion"></div>
 
           <div class="input-field">
-            <input type="password" name="pass1"  title="CONTRASEÑA CON NUMEROS, LETRAS MAYUSUCLAS Y MINUSUCLAS ENTRE 8 Y 15 CARACTERES" pattern="[A-Za-z0-9]{8,15}" id="pass1" required  >
+            <input type="password" name="pass1"  title="CONTRASEÑA CON NUMEROS, LETRAS MAYUSUCLAS Y MINUSUCLAS ENTRE 1 Y 15 CARACTERES" pattern="[A-Za-z0-9]{8,15}" id="pass1" required  >
             <label for="pass1">Contraseña:</label>
           </div>
 
@@ -75,6 +73,7 @@ include "../extend/header.php";
 </div>
 
 
+
 <?php $sel = $con->query("SELECT * FROM usuario ");
 $row = mysqli_num_rows($sel);
  ?>
@@ -112,7 +111,7 @@ $row = mysqli_num_rows($sel);
                 <button type="submit" class="btn-floating" ><i class="material-icons">repeat</i></button>
               </form>
               </td>
-              <td><img src="<?php echo $f['foto'] ?>" class="circle" style="width: 50px; height: 50px;" ></td>
+              <td><img src="<?php echo $f['foto'] ?>" style="width: 65px; height: 65px" class="circle"></td>
               <td>
                 <?php if ($f['bloqueo']==1): ?>
                   <a href="bloqueo_manual.php?us=<?php echo $f['idUsuario'] ?>&bl=<?php echo $f['bloqueo'] ?>"><i class="material-icons green-text">lock_open</i></a>
@@ -121,7 +120,25 @@ $row = mysqli_num_rows($sel);
                 <?php endif; ?>
               </td>
               <td>
-                <a href="eliminar_usuario.php" class="btn-floating red" onclick="swal({ title: 'Esta seguro que desea eliminar al usuario?', text: 'Al eliminarlo no podra recuperarlo!', type: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Si, eliminarlo!' }).then(function () {  location.href='eliminar_usuario.php?id=<?php echo $f['idUsuario'] ?>'; })"><i class="material-icons">clear</i></a>
+                <a href="#" class="btn-floating red" onclick="Swal.fire({
+                  title: '¿Está seguro que desea eliminar al usuario?',
+                  text: '¡Al eliminarlo no podrá recuperarlo!',
+                  icon: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#3085d6',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Sí, eliminarlo!',
+                  cancelButtonText: 'Cancelar'
+                }).then(function(result) {
+                  if (result.isConfirmed) {  // Solo redirige si se confirma
+                    location.href = 'eliminar_usuario.php?id=<?php echo $f['idUsuario'] ?>';
+                  } else {
+                    // Si se cancela, no hace nada (solo cierra el modal)
+                    return false;
+                  }
+                })">
+                  <i class="material-icons">clear</i>
+                </a>
               </td>
             </tr>
             <?php } ?>
@@ -132,7 +149,23 @@ $row = mysqli_num_rows($sel);
 </div>
 
 <?php include '../extend/scripts.php'; ?>
+<div class="fixed-action-btn vertical click-to-toggle">
+  <a class="btn-floating red">
+    <i class="material-icons" >mode_edit</i>
+  </a>
+  <ul>
+    <li><a href="" class="btn-floating yellow"><i class="material-icons">add</i></a></li>
+    <li><a href="" class="btn-floating orange"><i class="material-icons">replay</i></a></li>
+    <li><a href="" class="btn-floating purple"><i class="material-icons">repeat</i></a></li>
+    <li><a href="" class="btn-floating pink"><i class="material-icons">send</i></a></li>
+  </ul>
+</div>
 <script src="../js/validacion.js"></script>
-<script src="../js/scripts.js"></script>
+<script>
+  $('.fixed-action-btn').openFAB();
+</script>
+<script>
+  $('.button-collpase').sideNav();
+</script>
 </body>
 </html>
